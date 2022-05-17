4c549d2b80577db0609356a66611a0fb14c18485
Balaji TK <balajitkumar2@gmail.com>
2022-05-18 01:02:21 +0800

    swap without third variable

--- SelectionSort.py
+++ SelectionSort.py
@@ -7,9 +7,10 @@ for outer_index in range(len(lst_unsorted)-1):
         #inner_number = lst_unsorted[inner_index]
         if lst_unsorted[inner_index] < lst_unsorted[low_index]:
             low_index = inner_index
-    temp = lst_unsorted[low_index]
-    lst_unsorted[low_index] = lst_unsorted[outer_index]
-    lst_unsorted[outer_index] = temp
+    lst_unsorted[low_index],lst_unsorted[outer_index] = lst_unsorted[outer_index],lst_unsorted[low_index]
+    #temp = lst_unsorted[low_index]
+    #lst_unsorted[low_index] = lst_unsorted[outer_index]
+    #lst_unsorted[outer_index] = temp
 
 for int_number in lst_unsorted:
     print(int_number)
