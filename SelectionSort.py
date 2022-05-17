1c2e04abf714538ad80edffc8de1a4e9a8fecbfe
Balaji TK <balajitkumar2@gmail.com>
2022-05-18 00:54:07 +0800

    commit for selection sort

---
+++ SelectionSort.py
@@ -0,0 +1,17 @@
+lst_unsorted = [15,-1,36,20,2,0,-14,25]
+
+for outer_index in range(len(lst_unsorted)-1):
+    outer_number = lst_unsorted[outer_index]
+    low_index = outer_index
+    for inner_index in range(outer_index+1,len(lst_unsorted)):
+        #inner_number = lst_unsorted[inner_index]
+        if lst_unsorted[inner_index] < lst_unsorted[low_index]:
+            low_index = inner_index
+    temp = lst_unsorted[low_index]
+    lst_unsorted[low_index] = lst_unsorted[outer_index]
+    lst_unsorted[outer_index] = temp
+
+for int_number in lst_unsorted:
+    print(int_number)
+
+
